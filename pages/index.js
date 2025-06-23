21d21fe862fd3282bb45f8f0a400a131fa271006
TravisBadje <egorgorkovets00@gmail.com>
2025-06-23 19:25:07 +0500

    gfa

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 style={{ color: 'white' }}>GFA Social is live!</h1>
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <h1 className="text-white text-6xl font-bold">GFA</h1>
+    </div>
+  )
 }
